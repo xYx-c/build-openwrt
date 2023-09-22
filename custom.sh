4ba5615b246d039074f665cd85c0ac28e9626117
x <xYx.c@outlook.com>
2023-09-22 14:23:29 +0800

    ..

--- custom.sh
+++ custom.sh
@@ -17,11 +17,15 @@ sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
 # echo 'src-git xYx https://github.com/xYx-c/openwrt-luci' >>feeds.conf.default
 
 # openclash packages
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git OpenClash
-mv OpenClash/luci-app-openclash package/luci-app-openclash
-rm -rf OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git openclash
+mv openclash/luci-app-openclash package/luci-app-openclash && rm -rf openclash
 
-git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-adguardhome paakage/luci-app-adguardhome
+git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+git clone --depth=1 -b main https://github.com/kingyond/luci-app-accesscontrol-plus accesscontrol-plus
+mv accesscontrol-plus/luci-app-accesscontrol-plus  package/luci-app-accesscontrol-plus && rm -rf accesscontrol-plus
+
+git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-zerotier package/luci-app-zerotier
 
 # update golang
 # pushd feeds/packages/lang
